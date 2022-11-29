fde77cfdc68c6038dcada4981894aa94b2c06e81
matthijsdam <matthijs0894@hotmail.com>
2022-11-29 14:29:53 +0100

    Added 2020 day 1 and 2 solutions in Go

---
+++ 2020/day1.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	strconv "strconv"
+	"strings"
+)
+
+func day1() {
+	bytes, _ := os.ReadFile("2020/day1.txt")
+	puzzleInput := string(bytes)
+	lines := strings.Split(puzzleInput, "\n")
+	values := make([]int, len(lines))
+
+	for i, line := range lines {
+		values[i], _ = strconv.Atoi(line)
+	}
+
+	for _, value := range values {
+		for _, value2 := range values {
+			for _, value3 := range values {
+				if value+value2+value3 == 2020 {
+					fmt.Printf("%v * %v * %v = %v\n", value, value2, value3, value*value2*value3)
+				}
+			}
+		}
+	}
+}
+
+func main() {
+	day1()
+}
---
+++ 2020/day2.go
@@ -0,0 +1,58 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	strconv "strconv"
+	"strings"
+)
+
+type puzzleInputStruct struct {
+	minimum   int
+	maximum   int
+	character string
+	password  string
+}
+
+func day2() {
+	bytes, _ := os.ReadFile("2020/day2.txt")
+	puzzleInput := string(bytes)
+	lines := strings.Split(puzzleInput, "\n")
+	values := make([]puzzleInputStruct, len(lines))
+
+	for i, line := range lines {
+		parts := strings.Split(line, " ")
+		minMax := strings.Split(parts[0], "-")
+		values[i].minimum, _ = strconv.Atoi(minMax[0])
+		values[i].maximum, _ = strconv.Atoi(minMax[1])
+		values[i].character = strings.TrimSuffix(parts[1], ":")
+		values[i].password = parts[2]
+	}
+
+	correctAmountPart1 := 0
+	correctAmountPart2 := 0
+
+	for _, value := range values {
+		if strings.Count(value.password, value.character) >= value.minimum &&
+			strings.Count(value.password, value.character) <= value.maximum {
+			correctAmountPart1++
+		}
+
+		if string(value.password[value.minimum-1]) == value.character &&
+			string(value.password[value.maximum-1]) != value.character {
+			correctAmountPart2++
+		}
+
+		if string(value.password[value.minimum-1]) != value.character &&
+			string(value.password[value.maximum-1]) == value.character {
+			correctAmountPart2++
+		}
+	}
+
+	fmt.Println(correctAmountPart1)
+	fmt.Println(correctAmountPart2)
+}
+
+func main() {
+	day2()
+}
