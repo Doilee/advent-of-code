a22705de14e8efb70df459b6d0ae8f91aac11cc2
matthijsdam <matthijs0894@hotmail.com>
2022-12-01 09:36:32 +0100

    day 1 2022

--- 2022/day1.go
+++ 2022/day1.go
@@ -1,7 +1,48 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+	"sort"
+	"strconv"
+	"strings"
+)
 
 func main() {
-	fmt.Println("Hello world!")
+	bytes, _ := os.ReadFile("2022/day1.txt")
+	puzzleInput := string(bytes)
+	lines := strings.Split(puzzleInput, "\n\n")
+
+	answerPartOne := 0
+	var caloriesPerElf []int
+
+	for _, line := range lines {
+		calorieSum := 0
+		calories := strings.Split(line, "\n")
+
+		for _, calorie := range calories {
+			toInt, _ := strconv.Atoi(calorie)
+			calorieSum += toInt
+		}
+
+		if calorieSum > answerPartOne {
+			answerPartOne = calorieSum
+		}
+
+		caloriesPerElf = append(caloriesPerElf, calorieSum)
+	}
+	sort.Ints(caloriesPerElf)
+	highestThreeCalorieElves := caloriesPerElf[len(caloriesPerElf)-3:]
+	answerPartTwo := sum(highestThreeCalorieElves)
+
+	fmt.Println(answerPartOne)
+	fmt.Println(answerPartTwo)
+}
+
+func sum(array []int) int {
+	result := 0
+	for _, v := range array {
+		result += v
+	}
+	return result
 }
