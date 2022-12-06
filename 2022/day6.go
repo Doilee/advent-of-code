b43f07369b50fe5085106aeea479f16dd1e08e33
matthijsdam <matthijs0894@hotmail.com>
2022-12-06 14:45:49 +0100

    Day 6 answer in Go

---
+++ 2022/day6.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"fmt"
+	"os"
+)
+
+const PART_ONE = 4
+const PART_TWO = 14
+
+func main() {
+	bytes, _ := os.ReadFile("2022/day6.txt")
+	signal := string(bytes)
+
+	var lastFour []rune
+
+	for index, character := range signal {
+		unique := make(map[rune]bool, 26)
+		lastFour = append(lastFour, character)
+
+		if len(lastFour) > PART_ONE {
+			lastFour = lastFour[1:]
+		}
+
+		lastFourAreUnique := true
+		for _, char := range lastFour {
+
+			if unique[char] {
+				lastFourAreUnique = false
+				break
+			}
+
+			unique[char] = true
+		}
+
+		if lastFourAreUnique && len(lastFour) == PART_ONE {
+			fmt.Println(index + 1)
+			break
+		}
+	}
+}
