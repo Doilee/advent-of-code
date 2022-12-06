b847a7a0a0eecba041f4212e630e3c27cca289bf
matthijsdam <matthijs0894@hotmail.com>
2022-12-06 14:36:33 +0100

    Day 6 answer in PHP because I felt like it

---
+++ 2022/day6.php
@@ -0,0 +1,32 @@
+<?php
+
+$string = file_get_contents('day6.txt');
+
+
+const PART_ONE = 4;
+const PART_TWO = 14;
+
+$lastFour = [];
+
+foreach (str_split($string) as $index => $character) {
+    $lastFour[] = $character;
+
+    if (count($lastFour) > PART_TWO) {
+        array_shift($lastFour);
+    }
+
+    if (arrayHasDupes($lastFour)) {
+        var_dump(array_unique($lastFour));
+        var_dump($lastFour);
+        continue;
+    }
+
+    if (count($lastFour) == PART_TWO) {
+        var_dump('Answer part 1:' . $index + 1);
+        die();
+    }
+}
+
+function arrayHasDupes(array $array) : bool {
+    return count(array_unique($array)) !== count($array);
+}
